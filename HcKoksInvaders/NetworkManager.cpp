45c43e5a6e564883467463daecbaed2288feca57
Hannes Hauptmann <hannes.hauptmann@outlook.com>
2020-01-08 22:26:46 +0100

    sync  .... server ssl :P

--- HcKoksInvaders/NetworkManager.cpp
+++ HcKoksInvaders/NetworkManager.cpp
@@ -1,26 +1,154 @@
 #include "include/NetworkManager.hpp"
 
-#include <WinSock2.h>
-#include <iostream>
+#define WIN32_LEAN_AND_MEAN
 
-#pragma comment(lib, "ws2_32.lib")
+#ifndef UNICODE
+#define UNICODE
+#endif
 
-namespace {
-	static WSADATA wsadata;
-}
+#include <windows.h>
+#include <winsock2.h>
+#include <mstcpip.h>
+#include <ws2tcpip.h>
+#include <rpc.h>
+#include <ntdsapi.h>
+#include <stdio.h>
+#include <tchar.h>
+
+static WSADATA* wsadata = new WSADATA();;
 
 void NetworkManager::init() {
-	if (WSAStartup(0x0101, &wsadata))
-		std::cout << __FUNCSIG__ << " ERROR\n";
+    if (WSAStartup(0x0101, wsadata))
+        return;// std::cout << __FUNCSIG__ << " ERROR\n";
 }
 
 void NetworkManager::shutdown() {
-	if (WSACleanup())
-		std::cout << __FUNCSIG__ << " ERROR\n";
-	
+    if (WSACleanup())
+        return;// std::cout << __FUNCSIG__ << " ERROR\n";
+
 }
 
 void NetworkManager::sendHttpsPOSTRequest(const std::string url)
 {
+    const int RECV_DATA_BUF_SIZE = 2048;
+    const wchar_t* serverSPN = L"WWW/www.handata.eu";
 
+    sockaddr* serverAddr = (sockaddr*)new sockaddr_in();
+    constexpr size_t serverAddrLen = sizeof(*serverAddr);
+    SOCKET_SECURITY_SETTINGS* securitySettings = new SOCKET_SECURITY_SETTINGS();
+    constexpr size_t settingsLen = sizeof(*securitySettings);
+
+    securitySettings->SecurityProtocol = SOCKET_SECURITY_PROTOCOL_DEFAULT;
+    securitySettings->SecurityFlags = SOCKET_SETTINGS_ALLOW_INSECURE;
+
+    ((sockaddr_in*)serverAddr)->sin_family = AF_INET;
+    ((sockaddr_in*)serverAddr)->sin_port = htons(443);
+    ((sockaddr_in*)serverAddr)->sin_addr.S_un.S_addr = htonl((((((81 << 8) | 19) << 8) | 159) << 8) | 64);
+    // 81.19.159.64
+
+    int iResult = 0;
+    int sockErr = 0;
+    SOCKET sock = INVALID_SOCKET;
+
+    WSABUF wsaBuf = { 0 };
+    char* dataBuf = (char*)"GET index.html";
+    DWORD bytesSent = 0;
+    char recvBuf[RECV_DATA_BUF_SIZE] = { 0 };
+
+    DWORD bytesRecvd = 0;
+    DWORD flags = 0;
+    SOCKET_PEER_TARGET_NAME* peerTargetName = NULL;
+    DWORD serverSpnStringLen = (DWORD)wcslen(serverSPN);
+    DWORD peerTargetNameLen = sizeof(SOCKET_PEER_TARGET_NAME) +
+        (serverSpnStringLen * sizeof(wchar_t));
+
+    //-----------------------------------------
+    // Create a TCP socket
+    sock = WSASocket(serverAddr->sa_family,
+        SOCK_STREAM, IPPROTO_TCP, NULL, 0, 0);
+    if (sock == INVALID_SOCKET) {
+        iResult = WSAGetLastError();
+        wprintf(L"WSASocket returned error %ld\n", iResult);
+        goto cleanup;
+    }
+    //-----------------------------------------
+    // Turn on security for the socket.
+    sockErr = WSASetSocketSecurity(sock,
+        securitySettings, settingsLen, NULL, NULL);
+    if (sockErr == SOCKET_ERROR) {
+        iResult = WSAGetLastError();
+        wprintf(L"WSASetSocketSecurity returned error %ld\n", iResult);
+        goto cleanup;
+    }
+    //-----------------------------------------
+    // Specify the server SPN
+    peerTargetName = (SOCKET_PEER_TARGET_NAME*)HeapAlloc(GetProcessHeap(),
+        HEAP_ZERO_MEMORY, peerTargetNameLen);
+    if (!peerTargetName) {
+        iResult = ERROR_NOT_ENOUGH_MEMORY;
+        wprintf(L"Out of memory\n");
+        goto cleanup;
+    }
+    // Use the security protocol as specified by the settings
+    peerTargetName->SecurityProtocol = securitySettings->SecurityProtocol;
+    // Specify the server SPN 
+    peerTargetName->PeerTargetNameStringLen = serverSpnStringLen;
+    RtlCopyMemory((BYTE*)peerTargetName->AllStrings,
+        (BYTE*)serverSPN, serverSpnStringLen * sizeof(wchar_t)
+    );
+
+    sockErr = WSASetSocketPeerTargetName(sock,
+        peerTargetName,
+        peerTargetNameLen, NULL, NULL);
+    if (sockErr == SOCKET_ERROR) {
+        iResult = WSAGetLastError();
+        wprintf(L"WSASetSocketPeerTargetName returned error %ld\n", iResult);
+        goto cleanup;
+    }
+    //-----------------------------------------
+    // Connect to the server
+    sockErr = WSAConnect(sock,
+        serverAddr, serverAddrLen, NULL, NULL, NULL, NULL);
+    if (sockErr == SOCKET_ERROR) {
+        iResult = WSAGetLastError();
+        wprintf(L"WSAConnect returned error %ld\n", iResult);
+        goto cleanup;
+    }
+    // At this point a secure connection must have been established.
+    wprintf(L"Secure connection established to the server\n");
+
+    //-----------------------------------------
+    // Send some data securely
+    wsaBuf.len = (ULONG)strlen(dataBuf);
+    wsaBuf.buf = dataBuf;
+    sockErr = WSASend(sock, &wsaBuf, 1, &bytesSent, 0, NULL, NULL);
+    if (sockErr == SOCKET_ERROR) {
+        iResult = WSAGetLastError();
+        wprintf(L"WSASend returned error %ld\n", iResult);
+        goto cleanup;
+    }
+    wprintf(L"Sent %d bytes of data to the server\n", bytesSent);
+
+    //-----------------------------------------
+    // Receive server's response securely
+    wsaBuf.len = RECV_DATA_BUF_SIZE;
+    wsaBuf.buf = recvBuf;
+    sockErr = WSARecv(sock, &wsaBuf, 1, &bytesRecvd, &flags, NULL, NULL);
+    if (sockErr == SOCKET_ERROR) {
+        iResult = WSAGetLastError();
+        wprintf(L"WSARecv returned error %ld\n", iResult);
+        goto cleanup;
+    }
+    wprintf(L"Received %d bytes of data from the server\n", bytesRecvd);
+
+cleanup:
+    if (sock != INVALID_SOCKET) {
+        //This will trigger the cleanup of all IPsec filters and policies that
+        //were added for this socket. The cleanup will happen only after all
+        //outstanding data has been sent out on the wire.
+        closesocket(sock);
+    }
+    if (peerTargetName) {
+        HeapFree(GetProcessHeap(), 0, peerTargetName);
+    }
 }
